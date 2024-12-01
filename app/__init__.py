3953895d0ae115749c5832f7bc445beaac55cb32
Tufi2 <tufailsultan123@gmail.com>
2024-12-01 23:58:34 +0530

    working on chicken section

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,8 @@ login_manager.login_view = 'auth.login'
 mail = Mail()
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    app = Flask(__name__,
+            template_folder='templates' )
     app.config.from_object(config_class)
 
     # Initialize Flask extensions
